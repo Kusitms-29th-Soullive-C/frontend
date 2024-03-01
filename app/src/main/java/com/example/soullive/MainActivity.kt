8cbd3bcf9c758c942dc7f026f1b50363a70c3327
0se0 <030ggong@gmail.com>
2024-03-01 20:49:20 +0900

    Feat: nav연결 및 bnv숨기기

--- app/src/main/java/com/example/soullive/MainActivity.kt
+++ app/src/main/java/com/example/soullive/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.soullive
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.soullive.databinding.ActivityMainBinding
 
@@ -19,17 +21,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bnv.setupWithNavController(navController)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        hideBottomNavigationView(navController)
+    }
+
+    private fun hideBottomNavigationView(navController: NavController) {
+        navController.addOnDestinationChangedListener{ _, destination, _ ->
+            binding.bnv.visibility = when(destination.id){
+                R.id.splashFragment -> View.GONE
+                else -> View.VISIBLE
+            }
+        }
     }
 }
