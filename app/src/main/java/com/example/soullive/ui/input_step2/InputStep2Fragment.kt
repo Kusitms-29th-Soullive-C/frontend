3e0816cf0ce3c2376470e71b7933c0642bb8d3f4
bakhweebug <parksy8103@gmail.com>
2024-03-04 00:50:11 +0900

    Feat: 리사이클러뷰 삭제 기능 추가

--- app/src/main/java/com/example/soullive/ui/input_step2/InputStep2Fragment.kt
+++ app/src/main/java/com/example/soullive/ui/input_step2/InputStep2Fragment.kt
@@ -2,8 +2,6 @@ package com.example.soullive.ui.input_step2
 
 import android.content.Context
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/example/soullive/ui/input_step2/KeywordAdapter.kt
+++ app/src/main/java/com/example/soullive/ui/input_step2/KeywordAdapter.kt
@@ -3,6 +3,7 @@ package com.example.soullive.ui.input_step2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.soullive.R
@@ -10,9 +11,9 @@ import com.example.soullive.ui.input_step2.KeywordModel.Companion.KeywordList
 
 data class KeywordModel(
     val keyword: String,
-){
-    companion object{
-        val KeywordList = listOf(
+) {
+    companion object {
+        val KeywordList = mutableListOf(
             KeywordModel("프리미엄"),
             KeywordModel("프리미엄라인인 S시리즈 강조"),
             KeywordModel("새로 들어간 AI "),
@@ -21,15 +22,25 @@ data class KeywordModel(
     }
 }
 
-class KeywordAdapter(private val keywordList: List<KeywordModel>) :
+class KeywordAdapter(private val keywordList: MutableList<KeywordModel>) :
     RecyclerView.Adapter<KeywordAdapter.KeywordViewHolder>() {
     inner class KeywordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(keyword: KeywordModel) {
-            itemView.findViewById<TextView>(R.id.tv_keyword).text=keyword.keyword
+            itemView.findViewById<TextView>(R.id.tv_keyword).text = keyword.keyword
+            val deleteButton = itemView.findViewById<ImageButton>(R.id.btn_delete)
+            deleteButton.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    keywordList.removeAt(position)
+                    notifyItemRemoved(position)
+                    notifyItemRangeChanged(position, keywordList.size)
+                }
+            }
         }
     }
 
     override fun getItemCount(): Int = keywordList.size
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KeywordViewHolder {
         return KeywordViewHolder(
