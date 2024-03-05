1d2a50290993776fd50beb74eea60a10e56bedd5
bakhweebug <parksy8103@gmail.com>
2024-03-05 23:33:00 +0900

    Design: 이벤트팝업 페이지 XML

---
+++ app/src/main/java/com/example/soullive/ui/event/eventPopupFragment.kt
@@ -0,0 +1,37 @@
+package com.example.soullive.ui.event
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.soullive.databinding.FragmentEventPopupBinding
+import com.example.soullive.databinding.FragmentOnBoardingBinding
+
+class eventPopupFragment : Fragment() {
+
+    private var _binding: FragmentEventPopupBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEventPopupBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
