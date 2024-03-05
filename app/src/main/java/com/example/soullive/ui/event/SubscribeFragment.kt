16f841f41689687b5e2c457151858b7a25fb8f12
bakhweebug <parksy8103@gmail.com>
2024-03-06 02:03:19 +0900

    Design: 구독페이지 XML

---
+++ app/src/main/java/com/example/soullive/ui/event/SubscribeFragment.kt
@@ -0,0 +1,42 @@
+package com.example.soullive.ui.event
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.soullive.R
+import com.example.soullive.databinding.FragmentSubscribeBinding
+
+class SubscribeFragment : Fragment() {
+
+    private var _binding: FragmentSubscribeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSubscribeBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBackButton()
+    }
+    private fun setBackButton() {
+        binding.subscribeToolbar.setNavigationOnClickListener {
+            val navController = findNavController()
+            navController.navigateUp()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
